30ca0b19d3754462f27d8888ddc6d97ffb9dbd6c
王华 <wwwanghua@outlook.com>
2025-09-28 12:32:06 +0800

    Updated news.js

--- news/news.js
+++ news/news.js
@@ -6,7 +6,7 @@ function main() {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhhc2dxenRlYWt5anlqbnVtb3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5NzI3MDksImV4cCI6MjA1MTU0ODcwOX0.kDZGCdDbz1iwxB74wg8mVOqvZFjBB9piAW-B2cP992w'
     const resp = http.send(
         'GET',
-        `https://xasgqzteakyjyjnumosb.supabase.co/rest/v1/news?select=id,title,tags,url,image,content&draft=eq.false&offset=0&limit=30&order=created_at.asc`,
+        `https://xasgqzteakyjyjnumosb.supabase.co/rest/v1/news?select=id,title,tags,url,image,content&draft=eq.false&offset=0&limit=30&order=created_at.desc`,
         {
             apikey: apikey,
             Authorization: `Bearer ${bearer_token}`,
